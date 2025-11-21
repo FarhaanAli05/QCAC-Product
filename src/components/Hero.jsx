36324327a6a7295c1f200292b752ff39fa5b9e76
Benjamin Falkner <bfalkner9@gmail.com>
2025-11-21 11:38:13 -0500

    starter code

---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Product Name</h1>
+      <p>Your product tagline goes here.</p>
+    </section>
+  );
+}
